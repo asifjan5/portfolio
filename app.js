d1bcdaed17d5ad169b07fe0fa24d02ba9081a674
“MohammedTousif” <asifjan2786@gmail.com>
2025-06-02 00:31:48 +0530

    new web protfolio

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+// Set view engine
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("index", { name: "Asif" });
-});
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
+app.get("/projects", (req, res) => res.render("projects"));
+app.get("/contact", (req, res) => res.render("contact"));
+
+// Start server
 app.listen(port, () => {
   console.log(`Portfolio app listening at http://localhost:${port}`);
 });
